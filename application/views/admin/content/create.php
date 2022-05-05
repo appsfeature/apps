59a690a5878877271ffed2f53dbf78fec53eff60
droidhelios <droidhelios@gmail.com>
2022-05-05 11:45:53 +0530

    jhuk

--- application/views/admin/content/create.php
+++ application/views/admin/content/create.php
@@ -89,22 +89,22 @@
 
                         <div class="col-sm-4 mb-3">
                            <label for="ranking" class="formbuilder-number-label">Ranking</label>
-                           <input type="number" placeholder="Enter Ranking" class="form-control" name="ranking" access="false" value="0" id="ranking">
+                           <input type="number" placeholder="Enter here" class="form-control" name="ranking" access="false" value="0" id="ranking">
                         </div>
                     </div>
 
                     <div class="row">
                         <div class="col-sm-4 mb-3">
                             <label for="title" class="formbuilder-text-label">Title <span style="color:red">*</span></label>
-                            <input type="text" placeholder="Enter Title" class="form-control <?php echo (form_error('title') != "") ? 'is-invalid' : ''; ?>" name="title" access="false" id="title">
+                            <input type="text" placeholder="Enter here" class="form-control <?php echo (form_error('title') != "") ? 'is-invalid' : ''; ?>" name="title" access="false" id="title">
                         </div>
                         <div class="col-sm-4 mb-3">
-                            <label for="description" class="formbuilder-number-label">Description</label>
-                            <input type="text" placeholder="Enter description here" placeholder="Enter Description" class="form-control" name="description" access="false" id="description">
+                            <label for="description" class="formbuilder-number-label">Description / Html Body</label>
+                            <input type="text" placeholder="Enter here" class="form-control" name="description" access="false" id="description">
                         </div>
                         <div class="col-sm-4 mb-3">
-                            <label for="link" class="formbuilder-number-label">Link</label>
-                            <input type="text" placeholder="Enter Link" class="form-control" name="link" access="false" id="link">
+                            <label for="link" class="formbuilder-number-label">Link / PDF / VideoId</label>
+                            <input type="text" placeholder="Enter here" class="form-control" name="link" access="false" id="link">
                         </div>
                     </div>
                     <div class="row">
